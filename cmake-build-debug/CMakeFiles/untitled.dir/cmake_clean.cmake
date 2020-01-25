897407ae89a7b3d1d3a11be6d7406a3f51b4e116
Eliot Ostling <eostling@cnm.edu>
2020-01-24 21:58:20 -0500

    initial

---
+++ cmake-build-debug/CMakeFiles/untitled.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled.dir/library.cpp.obj"
+  "untitled.dll"
+  "untitled.dll.manifest"
+  "untitled.lib"
+  "untitled.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
