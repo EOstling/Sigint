f279f4541b4095e95b20cc9b1e9c230bd27bec92
Eliot Ostling <eostling@cnm.edu>
2020-01-26 10:13:37 -0500

    set the class layout of the Filterbank

--- FilterBank.h
+++ FilterBank.h
@@ -5,4 +5,19 @@
 #ifndef UNTITLED_FILTERBANK_H
 #define UNTITLED_FILTERBANK_H
 
+
+#include <string>
+using namespace std;
+
+class FilterBank{
+
+private:
+    int calculate(int *valArray);
+    long long mapMultiFB(char *s,int size, string *polyPhaseMetric);
+
+public:
+    void setCalcuate(int *val);
+
+};
+
 #endif //UNTITLED_FILTERBANK_H
