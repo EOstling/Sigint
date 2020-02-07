556557edb5b3cad7f5d3c0415610801bb7ed2793
Eliot Ostling <eostling@cnm.edu>
2020-02-07 16:59:32 -0500

    Finished headers

--- Search/Search.h
+++ Search/Search.h
@@ -23,6 +23,22 @@ public:
             int x2, int y2);
     bool Compare(const vector<int> a,
             const vector<int> b);
+
+    vector<vector<T>>ReadBoardFile(string filePath);
+
+    bool isValid(int row, int col);
+
+    void ExpandNeighbors(const vector<int> &current, int goal[2],
+            vector<vector<int>>&open,
+            vector<vector<T>>&grid);
+
+    bool CheckValidCell(int x, int y, vector<vector<T>>&grid);
+
+    void AddToOpen(int x, int y, int g, int h,
+            &vector<vector<int>>openNodes,
+            &vector<vector<State>grid>));
+
+    void CellSort(vector<vector<int>>*grid);
 };
 
 
